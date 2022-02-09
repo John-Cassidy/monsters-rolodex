341558142c1f390576851445e3dc7e084c136886
John Cassidy <jpcassidy@outlook.com>
2022-02-09 11:07:38 -0500

    add card and card-list components

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Built based on [Udemy Class](https://www.udemy.com/course/complete-react-develop
 ## Json Placeholder Data
 
 [Users](https://jsonplaceholder.typicode.com/users)
+
+## Monster Images
+
+[Example Image from Robohash.org](https://robohash.org/1?set=set2)
+
+[Another Example Image from Robohash.org](https://robohash.org/9?set=set2)
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 
+import { CardList } from './components/card-list/card-list.component';
 import { Component } from 'react';
 
 class App extends Component {
@@ -21,9 +22,7 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        {this.state.monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   }
